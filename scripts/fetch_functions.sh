320ecc367cc6ca39ae7e6ee4a802619e5f9df3a5
fabnicol <fabnicol@users.noreply.github.com>
2021-03-15 12:22:23 +0000

    license header

--- scripts/fetch_functions.sh
+++ scripts/fetch_functions.sh
@@ -1,3 +1,24 @@
+##
+# Copyright (c) 2020-2021 Fabrice Nicol <fabrnicol@gmail.com>
+#
+# This file is part of mkg.
+#
+# mkg is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+#
+# FFmpeg is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with FFmpeg; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301
+##
+
 #!/bin/bash
 
 ## @fn fetch_gentoo_install_iso()
